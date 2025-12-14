cb26510f9071d16a185afff9e33141998df646ba
a108petrischev-cell <a108petrischev@gmail.com>
2025-12-14 23:07:14 +0300

    еще пара правок

--- homework_08/config/urls.py
+++ homework_08/config/urls.py
@@ -18,8 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import store
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", include("store.urls")),
